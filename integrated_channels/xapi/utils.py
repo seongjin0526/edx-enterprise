2aa25badc10e962f9139d5c4170cbd98aa9ab05f
Saleem Latif <saleem_ee@hotmail.com>
2018-08-02 14:16:14 +0500

    Added management command to send course enrollment info.

--- integrated_channels/xapi/utils.py
+++ integrated_channels/xapi/utils.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, unicode_literals
 
 from integrated_channels.xapi.client import EnterpriseXAPIClient
 from integrated_channels.xapi.statements.learner_course_enrollment import LearnerCourseEnrollmentStatement
+from integrated_channels.xapi.serializers import CourseInfoSerializer, LearnerInfoSerializer
 
 
 def send_course_enrollment_statement(lrs_configuration, course_enrollment):
@@ -18,5 +19,13 @@ def send_course_enrollment_statement(lrs_configuration, course_enrollment):
          lrs_configuration (XAPILRSConfiguration): XAPILRSConfiguration instance where to send statements.
          course_enrollment (CourseEnrollment): Course enrollment object.
     """
-    statement = LearnerCourseEnrollmentStatement(course_enrollment.user, course_enrollment.course, {}, {})
+    user_details = LearnerInfoSerializer(course_enrollment.user)
+    course_details = CourseInfoSerializer(course_enrollment.course)
+
+    statement = LearnerCourseEnrollmentStatement(
+        course_enrollment.user,
+        course_enrollment.course,
+        user_details.data,
+        course_details.data,
+    )
     EnterpriseXAPIClient(lrs_configuration).save_statement(statement)
